ddf9ab2e19b54391d0c49367b0f704132550d9e4
Ivy <ivy@onprem.com>
2022-06-09 01:43:19 +0200

    Completed assignment.

---
+++ src/Eightball.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import './Eightball.css'
+
+const Eightball = ({ answers }) => {
+  const genRandom = () => {
+    const idx = Math.floor(Math.random() * answers.length)
+    return answers[idx]
+  }
+  
+  const newAnswer = () => {
+    const { msg, color } = genRandom()
+    setMsg(msg)
+    setColor(color)
+  }
+
+  const [msg, setMsg] = useState("Think of a question")
+  const [color, setColor] = useState("black")
+
+  return (
+    <div className="Eightball" style={{backgroundColor: color}} onClick={newAnswer}>
+      <p className="Eightball-msg">{msg}</p>
+    </div>
+  )
+}
+
+Eightball.defaultProps = {answers : [
+  { msg: "It is certain.", color: "green" },
+  { msg: "It is decidedly so.", color: "green" },
+  { msg: "Without a doubt.", color: "green" },
+  { msg: "Yes - definitely.", color: "green" },
+  { msg: "You may rely on it.", color: "green" },
+  { msg: "As I see it, yes.", color: "green" },
+  { msg: "Most likely.", color: "green" },
+  { msg: "Outlook good.", color: "green" },
+  { msg: "Yes.", color: "green" },
+  { msg: "Signs point to yes.", color: "goldenrod" },
+  { msg: "Reply hazy, try again.", color: "goldenrod" },
+  { msg: "Ask again later.", color: "goldenrod" },
+  { msg: "Better not tell you now.", color: "goldenrod" },
+  { msg: "Cannot predict now.", color: "goldenrod" },
+  { msg: "Concentrate and ask again.", color: "goldenrod" },
+  { msg: "Don't count on it.", color: "red" },
+  { msg: "My reply is no.", color: "red" },
+  { msg: "My sources say no.", color: "red" },
+  { msg: "Outlook not so good.", color: "red" },
+  { msg: "Very doubtful.", color: "red" },
+]}
+
+export default Eightball
